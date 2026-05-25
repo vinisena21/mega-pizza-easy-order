26f8cb4ebed595231747e039be363e0e521ee737
vinisena21 <viniciussena901@gmail.com>
2026-05-25 10:54:17 -0300

    Remove lovable watermark from vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,7 @@
-import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
-export default defineConfig();
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+})
